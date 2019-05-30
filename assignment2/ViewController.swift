70edc2044c74ad401c334b7794876e9cf9eab16a
arisamuto <cookiemonster.ca17@gmail.com>
2019-05-30 16:12:34 +0900

    Initial Commit

---
+++ assignment2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  assignment2
+//
+//  Created by Arisa Muto on 2019/05/30.
+//  Copyright © 2019 Arisa Muto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
